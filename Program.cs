f5cefaef9d5553c921ed0e4ac738f6a779616a02
Ziga <zlogar.ziga@gmail.com>
2020-07-11 21:36:13 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GuessingGame {
+    class Program {
+        static void Main(string[] args) {
+            var r = new Random();
+            int num = 0;
+            int target = r.Next(10);
+            Console.WriteLine("Pssst! its " + target);
+            while (true) {
+                Console.WriteLine("Guess a number:");
+                num = Convert.ToInt32(Console.ReadLine());
+
+                if (num == target) {
+                    Console.WriteLine("You have it!");
+                    break;
+                }
+                else if (num > target) {
+                    Console.WriteLine("Try lower");
+                }
+                else if (num < target) {
+                    Console.WriteLine("Try higher");
+                }
+            }
+        }
+    }
+}
